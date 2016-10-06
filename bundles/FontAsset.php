a047d32f1f49dc3a015bac97b107fe8ea6413c5f
Jiří Svoboda <jiri.svoboda@dlds.cz>
2016-10-06 14:39:16 +0200

    2.0.10

--- bundles/FontAsset.php
+++ bundles/FontAsset.php
@@ -13,7 +13,7 @@ class FontAsset extends AssetBundle
     public $sourcePath = '@dlds/metronic/assets';
 
     public $css = [
-        'http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all',
+        'https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all',
         'global/plugins/font-awesome/css/font-awesome.min.css',
     ];
 }
