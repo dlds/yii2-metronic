9956b1718bf5ed56e45eb71c5db1a65fd83323df
Sakurai Evsa <sakurai.evsa@gmail.com>
2017-02-04 01:16:45 +0700

    Fix undefined DatePickerAsset Fix DatePickerAsset js and css file location

--- bundles/DatePickerAsset.php
+++ bundles/DatePickerAsset.php
@@ -12,11 +12,11 @@ class DatePickerAsset extends BaseAssetBundle {
     public static $extraJs = [];
 
     public $js = [
-        'plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+        'global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
     ];
     public $css = [
-        'plugins/bootstrap-datepicker/css/bootstrap-datepicker.css',
-        'plugins/bootstrap-datepicker/css/bootstrap-datepicker3.css',
+        'global/plugins/bootstrap-datepicker/css/bootstrap-datepicker.css',
+        'global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.css',
     ];
     public $depends = [
         'dlds\metronic\bundles\CoreAsset',
@@ -25,6 +25,7 @@ class DatePickerAsset extends BaseAssetBundle {
     public function init()
     {
         $this->js = array_merge($this->js, static::$extraJs);
+         parent::init();
     }
 
 }
